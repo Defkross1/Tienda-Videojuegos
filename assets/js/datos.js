9984cf56b4ec895af16f691cf07cda3fda605835
Defkross1 <andresanhueza@gmail.com>
2026-07-07 22:24:58 -0400

    amplio base de datos a 16 productos con 4 variantes por plataforma

--- assets/js/datos.js
+++ assets/js/datos.js
@@ -1,7 +1,26 @@
 // Inventario base de la tienda de videojuegos
 const videojuegos = [
+    // --- PLATAFORMA: PS5 ---
     { plataforma: "PS5", nombre: "EA Sports FC 26", clasificacion: "Premium", precio: 69990, stock: 8 },
+    { plataforma: "PS5", nombre: "Marvel's Spider-Man 2", clasificacion: "Premium", precio: 64990, stock: 5 },
+    { plataforma: "PS5", nombre: "God of War Ragnarök", clasificacion: "Premium", precio: 59990, stock: 12 },
+    { plataforma: "PS5", nombre: "Resident Evil 4 Remake", clasificacion: "Precio Medio", precio: 39990, stock: 3 },
+
+    // --- PLATAFORMA: PC ---
     { plataforma: "PC", nombre: "Minecraft", clasificacion: "Económico", precio: 29990, stock: 15 },
-    { plataforma: "Nintendo Switch", nombre: "Mario Kart 8", clasificacion: "Precio Medio", precio: 59990, stock: 4 },
-    { plataforma: "Xbox", nombre: "Forza Horizon 5", clasificacion: "Precio Medio", precio: 54990, stock: 0 }
+    { plataforma: "PC", nombre: "Cyberpunk 2077", clasificacion: "Precio Medio", precio: 34990, stock: 20 },
+    { plataforma: "PC", nombre: "Elden Ring", clasificacion: "Premium", precio: 49990, stock: 7 },
+    { plataforma: "PC", nombre: "Stardew Valley", clasificacion: "Económico", precio: 7500, stock: 50 },
+
+    // --- PLATAFORMA: NINTENDO SWITCH ---
+    { plataforma: "Nintendo Switch", nombre: "Mario Kart 8 Deluxe", clasificacion: "Precio Medio", precio: 59990, stock: 4 },
+    { plataforma: "Nintendo Switch", nombre: "The Legend of Zelda: Tears of the Kingdom", clasificacion: "Premium", precio: 69990, stock: 6 },
+    { plataforma: "Nintendo Switch", nombre: "Super Mario Bros. Wonder", clasificacion: "Precio Medio", precio: 54990, stock: 9 },
+    { plataforma: "Nintendo Switch", nombre: "Pokémon Scarlet", clasificacion: "Precio Medio", precio: 49990, stock: 0 }, // Agotado para probar dinámica
+
+    // --- PLATAFORMA: XBOX ---
+    { plataforma: "Xbox", nombre: "Forza Horizon 5", clasificacion: "Precio Medio", precio: 54990, stock: 0 }, // Original de tu captura
+    { plataforma: "Xbox", nombre: "Halo Infinite", clasificacion: "Económico", precio: 24990, stock: 14 },
+    { plataforma: "Xbox", nombre: "Gears 5", clasificacion: "Económico", precio: 19990, stock: 8 },
+    { plataforma: "Xbox", nombre: "Starfield", clasificacion: "Premium", precio: 65990, stock: 11 }
 ];
