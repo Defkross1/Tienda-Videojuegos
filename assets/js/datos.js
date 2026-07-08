63ef5929e1bbafc93400635d0dffa40b0ef51483
Defkross1 <andresanhueza@gmail.com>
2026-07-07 20:51:05 -0400

    estructura inicial del proyecto y configuracion de carpetas

---
+++ assets/js/datos.js
@@ -0,0 +1,27 @@
+// Inventario base de la tienda de videojuegos
+let inventarioVideojuegos = [
+  {
+    nombre: "EA Sports FC 26",
+    plataforma: "PS5",
+    precio: 69990,
+    stock: 8
+  },
+  {
+    nombre: "Minecraft",
+    plataforma: "PC",
+    precio: 29990,
+    stock: 15
+  },
+  {
+    nombre: "Mario Kart 8",
+    plataforma: "Nintendo Switch",
+    precio: 59990,
+    stock: 4
+  },
+  {
+    nombre: "Forza Horizon 5",
+    plataforma: "Xbox",
+    precio: 54990,
+    stock: 0
+  }
+];
