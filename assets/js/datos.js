71c2a82e49b60048f9df9b45201871ad666ad6f1
Defkross1 <andresanhueza@gmail.com>
2026-07-07 21:59:45 -0400

    definir estructura de datos estática para los 4 videojuegos del inventario

--- assets/js/datos.js
+++ assets/js/datos.js
@@ -1,27 +1,7 @@
 // Inventario base de la tienda de videojuegos
-let inventarioVideojuegos = [
-  {
-    nombre: "EA Sports FC 26",
-    plataforma: "PS5",
-    precio: 69990,
-    stock: 8
-  },
-  {
-    nombre: "Minecraft",
-    plataforma: "PC",
-    precio: 29990,
-    stock: 15
-  },
-  {
-    nombre: "Mario Kart 8",
-    plataforma: "Nintendo Switch",
-    precio: 59990,
-    stock: 4
-  },
-  {
-    nombre: "Forza Horizon 5",
-    plataforma: "Xbox",
-    precio: 54990,
-    stock: 0
-  }
+const videojuegos = [
+    { plataforma: "PS5", nombre: "EA Sports FC 26", clasificacion: "Premium", precio: 69990, stock: 8 },
+    { plataforma: "PC", nombre: "Minecraft", clasificacion: "Económico", precio: 29990, stock: 15 },
+    { plataforma: "Nintendo Switch", nombre: "Mario Kart 8", clasificacion: "Precio Medio", precio: 59990, stock: 4 },
+    { plataforma: "Xbox", nombre: "Forza Horizon 5", clasificacion: "Precio Medio", precio: 54990, stock: 0 }
 ];
